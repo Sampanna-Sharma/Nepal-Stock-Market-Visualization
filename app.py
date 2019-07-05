abc69ff4da556be67f29fa7a653bb6c564305d50
Sampanna-Sharma <073bct538.sampanna@pcampus.edu.np>
2019-07-05 21:40:27 +0545

    version 0.0.2

--- app.py
+++ app.py
@@ -1,130 +1,77 @@
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, CustomJS, Select, HoverTool, DatetimeTickFormatter
-from bokeh.layouts import row, column
+from bokeh.models import ColumnDataSource
 from bokeh.embed import components
 from bokeh.resources import CDN
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+
 import pandas as pd
 import numpy as np
-from bokeh.palettes import Category20
 from flask_cors import CORS
+from plot import candle_plot
 
-#palletes
-RED = Category20[7][6]
-GREEN = Category20[5][4]
-
-BLUE = Category20[3][0]
-BLUE_LIGHT = Category20[3][1]
-
-ORANGE = Category20[3][2]
-PURPLE = Category20[9][8]
-BROWN = Category20[11][10]
-
-#Default Data
 
 data = pd.read_csv("stockprice_15_04_2019-25_10_2017.csv")
-company_data = data.loc[data['traded_companies'] == 'Nabil Bank Limited']
-
-company_data["date"] = pd.to_datetime(company_data['date'], cache = True)
-company_date = np.array(company_data["date"],dtype = np.datetime64).tolist()
-c = []
-for op,cl in zip(company_data["previous_closing"], company_data["closing_price"]):
-    if cl > op:
-        c.append(GREEN)
-    elif cl< op:
-        c.append(RED)
-    else:
-        c.append(BLUE)
 
 
-nabil_data = dict(  time = company_date,
-                    open = company_data["previous_closing"].to_list(),
-                    close = company_data["closing_price"].to_list(),
-                    high = company_data["max_price"].to_list(),
-                    low = company_data["min_price"].to_list(),
-                    color = c)
-
-source = ColumnDataSource(nabil_data)
-
-
-
-
-# widgets
-TOOLS = "pan,xwheel_zoom,reset,hover"
 
 
 app = Flask(__name__)
 CORS(app)
 
-def plot():
 
-
-    p = figure(x_axis_type="datetime",title = "CandleStick",plot_width=1200, plot_height=500,
-          tools=TOOLS, active_scroll = 'xwheel_zoom', toolbar_location = "above")
-
-    p.xaxis.formatter = DatetimeTickFormatter(
-                                days=["%F"],
-                                months=["%F"],
-                                years=["%F"],)
-    p.xaxis.axis_label = "date"
-    p.yaxis.axis_label = "price"
-
-    p.segment(x0='time', y0='low', x1='time', y1='high', line_width=1, color='black', source=source)
-    p.segment(x0='time', y0='open', x1='time', y1='close', line_width=6, color='color', source=source)
-    p.line(x='time', y='close', alpha = 0.5, color = ORANGE, source = source)
-
-    hover = p.select(dict(type=HoverTool))
-    hover.tooltips = [
-        ("Date", "@time{%F}"),
-        ("Open", "@open"),
-        ("Close", "@close"),
-        ("High", "@high"),
-        ("Low", "@low"),
-        ]
-    hover.formatters={"@time":'datetime',}
-    hover.mode = "vline"
-
-    callback = CustomJS(args=dict(source=source), code="""
-        var company_name = cb_obj.value
-        var data = source.data;
-        let xhr  = new XMLHttpRequest();
-        var url = "http://localhost:3000/data?company="+company_name.replace(/ /g,"%20")
-        xhr.open('GET',url);
-        xhr.send();
-        response = []
-        console.log(url)
-        xhr.onload = function(){
-            var response = xhr.response;
-            response = JSON.parse(response);
-            console.log(response)
-            
-        
-        data['color'] = response.color
-        data['open'] = response.open
-        data['close'] = response.close
-        data['high'] = response.high
-        data['low'] = response.low
-        data['time'] = response.time
-        
-        source.change.emit();
-        console.log("check",data)}
-        """)
-
-
-    #Selection
-    option = list(set(data.traded_companies.dropna()))
-    Company_select = Select(value="Nabil Bank Limited", options=option)
-    Company_select.js_on_change('value', callback)
-
-    layout = column(Company_select, p)
-    return layout
 
 @app.route('/')
 def index():
-	plt = plot()
+	plt = candle_plot(data.traded_companies.dropna())
 
 	script, div = components(plt)
 	return render_template("index.html", script=script, div=div)
+
+@app.route('/data', methods = ['GET'])
+def data_serve():
+    company_name = request.args.get('company')
+    print("here",company_name)
+    company_data = data.loc[data['traded_companies'] == company_name]
+    company_data["date"] = pd.to_datetime(company_data['date'], cache = True)
+    company_date = np.array(company_data["date"],dtype = np.datetime64).tolist()
+    company_date = list(map(lambda x: x/1000000,company_date))
+    company_close = np.array(company_data["closing_price"]).tolist()
+    company_open = np.array(company_data["previous_closing"]).tolist()
+    company_diff = np.array(company_data["difference_rs"]).tolist()
+    company_high = np.array(company_data["max_price"]).tolist()
+    company_low = np.array(company_data["min_price"]).tolist()
+
+    company_ma20 = company_data['closing_price'].rolling(20).mean().fillna(0).to_numpy()
+    company_ma50 = company_data['closing_price'].rolling(50).mean().fillna(0).to_numpy()
+    company_std20 = company_data['closing_price'].rolling(20).std().fillna(0).to_numpy()
+    company_mad = (company_ma20 - company_ma50).tolist()
+    company_bband_u = (company_ma20 + 2 * company_std20).tolist()
+    company_bband_l = (company_ma20 - 2 * company_std20).tolist()
+ 
+    c = []
+    for op,cl in zip(company_open, company_close):
+        if cl > op:
+            c.append("green")
+        elif cl< op:
+            c.append("red")
+        else:
+            c.append("blue")
+
+    response = {
+            'color' : c,
+            'time' : company_date,
+            'open' : company_open,
+            'close' : company_close,
+            'high' : company_high,
+            'low' : company_low,
+            'ma20' : company_ma20.tolist(),
+            'ma50' : company_ma50.tolist(),
+            'mad' : company_mad,
+            'std20': company_std20.tolist(),
+            'bband_u' : company_bband_u,
+            'bband_l' : company_bband_l,
+    }
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     app.run(debug = True)
---
+++ plot.py
@@ -0,0 +1,134 @@
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, CustomJS, Select, HoverTool, DatetimeTickFormatter, Band,Panel, Tabs
+from bokeh.layouts import row, column
+import pandas as pd
+import numpy as np
+from bokeh.palettes import Category20
+#palletes
+RED = Category20[7][6]
+GREEN = Category20[5][4]
+
+BLUE = Category20[3][0]
+BLUE_LIGHT = Category20[3][1]
+
+ORANGE = Category20[3][2]
+PURPLE = Category20[9][8]
+BROWN = Category20[11][10]
+
+
+TOOLS = "pan,xwheel_zoom,reset,hover"
+data = pd.read_csv("stockprice_15_04_2019-25_10_2017.csv")
+company_data = data.loc[data['traded_companies'] == 'Nabil Bank Limited']
+company_data["date"] = pd.to_datetime(company_data['date'], cache = True)
+company_date = np.array(company_data["date"],dtype = np.datetime64).tolist()
+c = []
+for op,cl in zip(company_data["previous_closing"], company_data["closing_price"]):
+    if cl > op:
+        c.append(GREEN)
+    elif cl< op:
+        c.append(RED)
+    else:
+        c.append(BLUE)
+
+
+nabil_data = dict(  time = company_date,
+                    open = company_data["previous_closing"].to_list(),
+                    close = company_data["closing_price"].to_list(),
+                    high = company_data["max_price"].to_list(),
+                    low = company_data["min_price"].to_list(),
+                    color = c,
+                    no_trans = company_data["no_of_transaction"].to_list(),
+                    ma20 = company_data['closing_price'].rolling(20).mean().to_list(),
+                    ma50 = company_data['closing_price'].rolling(50).mean().to_list(),
+                    std20 = company_data['closing_price'].rolling(20).std().to_list(),
+                    mad = (company_data['closing_price'].rolling(20).mean() - company_data['closing_price'].rolling(50).mean().fillna(0)).to_list(),
+                    bband_u = (company_data['closing_price'].rolling(20).mean() + 2 * company_data['closing_price'].rolling(20).std().fillna(0)).to_list(),
+                    bband_l = (company_data['closing_price'].rolling(20).mean() - 2 * company_data['closing_price'].rolling(20).std().fillna(0)).to_list(),
+                    )
+
+
+
+
+def candle_plot(traded_companies):
+
+    source = ColumnDataSource(nabil_data)
+
+    p = figure(x_axis_type="datetime",title = "CandleStick",plot_width=1200, plot_height=500,
+          tools=TOOLS, active_scroll = 'xwheel_zoom', toolbar_location = "above")
+
+    p.xaxis.formatter = DatetimeTickFormatter(
+                                days=["%F"],
+                                months=["%F"],
+                                years=["%F"],)
+    p.xaxis.axis_label = "date"
+    p.yaxis.axis_label = "price"
+
+    p.segment(x0='time', y0='low', x1='time', y1='high', line_width=1, color='black', source=source)
+    p.segment(x0='time', y0='open', x1='time', y1='close', line_width=6, color='color', source=source)
+    p.line(x='time', y='close', alpha = 0.5, color = ORANGE, source = source)
+
+    hover = p.select(dict(type=HoverTool))
+    hover.tooltips = [
+        ("Date", "@time{%F}"),
+        ("Open", "@open"),
+        ("Close", "@close"),
+        ("High", "@high"),
+        ("Low", "@low"),
+        ]
+    hover.formatters={"@time":'datetime',}
+    hover.mode = "vline"
+
+    callback = CustomJS(args=dict(source=source), code="""
+        var company_name = cb_obj.value
+        var data = source.data;
+        let xhr  = new XMLHttpRequest();
+        var url = "http://localhost:5000/data?company="+company_name.replace(/ /g,"%20")
+        xhr.open('GET',url);
+        xhr.send();
+        response = []
+        console.log(url)
+        xhr.onload = function(){
+            var response = xhr.response;
+            response = JSON.parse(response);
+            console.log(response)
+            
+        data['color'] = response.color
+        data['open'] = response.open
+        data['close'] = response.close
+        data['high'] = response.high
+        data['low'] = response.low
+        data['time'] = response.time
+        data['ma20'] = response.ma20
+        data['ma50'] = response.ma50
+        data['mad'] = response.mad
+        data['std20'] = response.std20
+        data['bband_u'] = response.bband_u
+        data['bband_l'] = response.bband_l
+        source.change.emit();}
+        """)
+
+
+    #Selection
+    option = ["Nabil Bank Limited", "Bank of Asia Nepal Limited", "Arun Valley Hydropower Development Co. Ltd.",
+                "Citizen Bank International Limited", "Bank of Kathmandu Ltd.", "Nepal Bangladesh Bank Limited"]
+    Company_select = Select(value="Nabil Bank Limited", options=option)
+    Company_select.js_on_change('value', callback)
+
+
+    #second plot    
+    p4 = figure(x_axis_type="datetime",active_scroll = 'xwheel_zoom',title = "Bollinger Band",plot_width=1200, plot_height=500, tools=TOOLS+ ',crosshair')
+    p4.line(x='time', y='ma20', color=RED, alpha = 0.6,source=source, legend = "Moving Average :- 20")
+    p4.line(x='time', y='close', color='black', line_width = 2,source=source, legend = "closing_price")
+
+    band = Band(base='time', lower='bband_l', upper='bband_u', source=source, level='underlay',
+                    fill_alpha=0.5, line_width=1, line_color='black', fill_color=BLUE_LIGHT)
+    p4.add_layout(band)
+
+    tab1 = Panel(child=p, title="candle stick")
+    tab2 = Panel(child=p4, title="bollinder")
+
+    tabs = Tabs(tabs=[ tab1, tab2 ])
+
+    layout = column(Company_select,tabs)
+
+    return layout
